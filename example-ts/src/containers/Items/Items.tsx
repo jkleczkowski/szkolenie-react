d4fd500d987c98d15f8f873b2c05fd116277965e
Jacek Kleczkowski <Jacek.Kleczkowski@mbank.pl>
2018-04-24 12:10:09 +0200

    constanses multi

--- example-ts/src/containers/Items/Items.tsx
+++ example-ts/src/containers/Items/Items.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import DataGrid from '../../coponents/DataGrid'
 import { connect } from "react-redux";
-import { ADD_ITEM } from "../../utils/contrans";
+import * as c from "../../utils/contrans";
 export interface IProps {
     data: any[],
     addItem: any
@@ -32,7 +32,7 @@ export default connect(
     (dispatch) => {
         return {
             addItem() {
-                return dispatch({ type: ADD_ITEM, payload: Date.now() });
+                return dispatch({ type: c.ADD_ITEM, payload: Date.now() });
             }
         }
     }
