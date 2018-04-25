e7e7a410ea758942bc6662b7153e34d8420e2d72
Jacek Kleczkowski <Jacek.Kleczkowski@mbank.pl>
2018-04-25 11:21:08 +0200

    Login Modal

--- example-ts/src/coponents/ModalGenerator/ModalGenerator.tsx
+++ example-ts/src/coponents/ModalGenerator/ModalGenerator.tsx
@@ -1,21 +1,44 @@
 import * as React from "react"
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
 export interface IProps {
     children?: React.ReactNode
 }
 
-export interface IState { }
+export interface IState {
+    modal: boolean
+}
 
 export default class ModalGenerator extends React.Component<IProps, IState> {
 
     constructor(props: IProps) {
         super(props)
 
-        this.state = {}
+        this.state = { modal: false }
+        this.toggle = this.toggle.bind(this);
+    }
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
     }
 
-    public render() {
+
+    render() {
         return (
-            <div>{this.props.children} </div>
+            <div>
+                <Button color="danger" onClick={this.toggle}>log in</Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} >
+                    <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                    <ModalBody>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
         )
     }
 }
