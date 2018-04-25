ea6e7403855f28186724f1955e7877dc0d898049
Jacek Kleczkowski <Jacek.Kleczkowski@mbank.pl>
2018-04-25 11:38:53 +0200

    standaryzacja ModalGenerator

--- example-ts/src/coponents/ModalGenerator/ModalGenerator.tsx
+++ example-ts/src/coponents/ModalGenerator/ModalGenerator.tsx
@@ -2,7 +2,9 @@ import * as React from "react"
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 export interface IProps {
-    children?: React.ReactNode
+    children?: React.ReactNode;
+    title: string;
+    buttonLabel: string;
 }
 
 export interface IState {
@@ -27,12 +29,12 @@ export default class ModalGenerator extends React.Component<IProps, IState> {
     render() {
         return (
             <div>
-                <Button color="danger" onClick={this.toggle}>log in</Button>
+                <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
                 <Modal isOpen={this.state.modal} toggle={this.toggle} >
-                    <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                    <ModalHeader toggle={this.toggle}>{this.props.title}</ModalHeader>
                     <ModalBody>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </ModalBody>
+                        {this.props.children}
+                    </ModalBody>
                     <ModalFooter>
                         <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
                         <Button color="secondary" onClick={this.toggle}>Cancel</Button>
